732301b1f630d62cf092c80b540ccf95e56ea886
s27281 <s27281@pjwstk.edu.pl>
2026-03-28 15:58:36 +0100

    Zjazd 3 , zadanie 3

--- Zjazd3/sandbox/src/main/java/pl/CypCzer/sandbox/TestController.java
+++ Zjazd3/sandbox/src/main/java/pl/CypCzer/sandbox/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable; //Zadanie 3.3
 
 @RestController
 @RequestMapping("/test")
@@ -19,5 +20,11 @@ public class TestController {
     public ResponseEntity<Car> getCarModel() {
         Car myCar = new Car("Tesla", "Model 3", 2024);
         return ResponseEntity.ok(myCar);
+    }
+
+    // Zadanie 3.3
+    @GetMapping("/hello/{someValue}")
+    public ResponseEntity<String> getDynamicValue(@PathVariable String someValue) {
+        return ResponseEntity.ok(someValue);
     }
 }
